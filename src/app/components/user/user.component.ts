edec7ebda9a91a210df0b692e2ba81b200ee52b5
Dogia <danielosorioorozco@gmail.com>
2021-01-21 10:20:20 -0500

    Se crea el componente con la información básica del usuario y que permite cambiar la contraseña

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { USER } from 'src/app/services/user.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+
+  public imageprofileURL: string = '#';
+  public user: any = {};
+
+  public password: string = '';
+  public repassword: string = '';
+
+  constructor(private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.imageprofileURL =  `http://apibitwanv1.tk/public/uploads/${environment.applicantcode}/users/${USER.getId()}/${USER.getImage()}`;
+    this.loadData();
+  }
+
+  async loadData(){
+    const response = await USER.verPorId(USER.getId());
+    this.user = response.data;
+    console.log(response);
+  }
+
+  async cambirClave(){
+    if (this.password.length > 7 && this.password == this.repassword){
+      const response = await USER.cambiarClave(this.password);
+      this.toastr.info(response.msg, 'Nueva contraseña');
+      this.password = '';
+      this.repassword = '';
+    }else{
+      if (this.password.length < 8)
+        this.toastr.info('Tu contraseña debe contener 8 caracteres al menos.', 'Nueva contrasñea');
+      if (this.password != this.repassword){
+        this.toastr.info('Las contraseñas no coinciden', 'Nueva contrasñea')
+      }
+    }
+    
+  }
+}

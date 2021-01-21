5ccf47ded25d9336d89f5b05c6e5e84ac5d9738a
Dogia <danielosorioorozco@gmail.com>
2021-01-21 00:04:46 -0500

    Variables de entorno se agrega la ruta update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,30 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  applicantcode: 461589,
+  URLs: {
+    usuarios:{
+      login: 'http://apibitwanv1.tk/public/login',
+      verPorId: 'http://apibitwanv1.tk/public/users/viewbyid',
+      crear: 'http://apibitwanv1.tk/public/users/create',
+      editar: 'http://apibitwanv1.tk/public/users/edit',
+      cambiarPassword: 'http://apibitwanv1.tk/public/users/changepassword'
+    },
+    publicaciones: {
+      verPorId: 'http://apibitwanv1.tk/public/posts/viewbyid',
+      verPorUsuarioId: 'http://apibitwanv1.tk/public/posts/viewbyiduser',
+      crear: 'http://apibitwanv1.tk/public/posts/create',
+      editar: 'http://apibitwanv1.tk/public/posts/edit',
+      eliminar: 'http://apibitwanv1.tk/public/posts/delete'
+    },
+    comentarios: {
+      verPorPostId: 'http://apibitwanv1.tk/public/comments/viewbyidpost',
+      crear: 'http://apibitwanv1.tk/public/comments/create',
+      eliminar: 'http://apibitwanv1.tk/public/comments/delete'
+    },
+    uploads: 'http://apibitwanv1.tk/public/uploads'
+  }
 };
 
 /*
